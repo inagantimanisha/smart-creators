b3836873889be5fc76d9bf42dd6293cf51578e37
manishainaganti <inagantimanisha60@gmail.com>
2025-07-30 13:45:11 +0530

    uploading python file

---
+++ app.py
@@ -0,0 +1,12 @@
+ame="MLEWC"
+number=10.0
+iSOn=True
+letter="H"
+#arithematic- + - * / %
+#logical - and,or,not
+#comparison - <, ==, !=, >= ,<=
+#assignment - =,+=,-=,*=,/=
+if (number>5):
+    print(name)
+else:
+    print(iSOn)
